7183d9e6dbf98430dfeeba887ca69c3f33d76da7
ethanbhuang <ethanbhuang@gmail.com>
2020-02-09 11:30:55 -0500

    added verification pages

--- src/page_manager.py
+++ src/page_manager.py
@@ -2,21 +2,19 @@ import kivy
 from kivy.app import App
 import os
 
-from kivy.lang import Builder
+import json
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import StringProperty
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 
-class RoundedButton(Button):
-    pass
-
-# home
+# home page
 class signInPage(BoxLayout):
     def __init__(self, **kwargs):
-        super(signInPage, self).__init__(**kwargs)
+        super(signInPage,self).__init__(**kwargs)
         self.cols = 2
         buttons = [
             {
@@ -39,8 +37,7 @@ class signInPage(BoxLayout):
                 background_down=button["bg_pressed"],
                 font_size=30,
                 color=(0,0,0,1),
-                # size_hint=(0.4,0.15),
-                size=(100,20),
+                size_hint=(1,0.15),
             )
             self.button.bind(on_press=button["callback"])
             self.add_widget(self.button)
@@ -60,6 +57,43 @@ class loginPage(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.col = 1
+        self.widgets = []
+        header = Label(
+            text="Log in",
+            color=(0.96, 0.65, 0.25,1),
+            font_size=30,
+        )
+        self.widgets.append(header)
+        field_labels = [
+            "Username",
+            "Password",
+        ]
+        for label in field_labels:
+            self.widgets.append(
+                Label(text=label, font_size=14,)
+            )
+            self.widgets.append(
+                TextInput(multiline=False)
+            )
+
+        login_button = Button(text="Log in",
+        background_normal="assets/light_turqoise.jpg",
+        background_down="assets/dark_turqoise.jpg")
+        login_button.bind(on_press=self.verifyUser)
+        self.widgets.append(login_button)
+
+        for widget in self.widgets:
+            self.add_widget(widget)
+
+    def verifyUser(self, *args):
+        with open("users.json", "r") as users_file:
+            users = json.load(users_file)
+            for user in users["users"]:
+                if user["username"] == self.widgets[2].text:
+                    if user["password"] == self.widgets[4].text:
+                        print("Access granted")
+                else:
+                    print("Access denied")
 
 class registerPage(BoxLayout):
     def __init__(self, **kwargs):
@@ -86,7 +120,7 @@ class registerPage(BoxLayout):
                 background_down=button["bg_pressed"],
                 font_size=30,
                 color=(0,0,0,1),
-                size_hint=(0.4,1),
+                size_hint=(0.5,0.15),
             )
             self.button.bind(on_press=button["callback"])
             self.add_widget(self.button)
@@ -106,13 +140,13 @@ class registerDonorPage(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.col = 1
-        widgets = []
+        self.widgets = []
         header = Label(
             text="Register as a donor\n or a volunteer",
             color=(0.96, 0.65, 0.25,1),
             font_size=30,
         )
-        widgets.append(header)
+        self.widgets.append(header)
         field_labels = [
             "Full Name",
             "Profile Summary",
@@ -122,20 +156,104 @@ class registerDonorPage(BoxLayout):
             "Password Again",
         ]
         for label in field_labels:
-            widgets.append(
+            self.widgets.append(
                 Label(text=label, font_size=14)
             )
-            widgets.append(
+            self.widgets.append(
                 TextInput()
             )
 
-        for widget in widgets:
+        register_button = Button(text="Register")
+        register_button.bind(on_press=self.registerDonor)
+        self.widgets.append(register_button)
+
+        for widget in self.widgets:
             self.add_widget(widget)
+
+    def registerDonor(self, *args):
+        with open("users.json", "r") as users_file:
+            users = json.load(users_file)
+
+            if self.widgets[2].text in users["users"]:
+                print("user already exists")
+            elif self.widgets[10].text == self.widgets[12].text:
+                with open("users.json", "w") as users_file:
+                    users["users"].append(
+                        {
+                            "username":self.widgets[2].text,
+                            "profile_summary":self.widgets[4].text,
+                            "volunteer_experience":self.widgets[6].text,
+                            "email_address":self.widgets[8].text,
+                            "password":self.widgets[10].text,
+                            "type":"DONOR"
+                        }
+                    )
+                    json.dump(users, users_file)
 
 class registerOrgPage(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        pass
+        self.cols = 2
+        self.widgets = []
+        header = Label(
+            text="Register as a donor\n or a volunteer",
+            color=(0.96, 0.65, 0.25,1),
+            font_size=30,
+        )
+        self.widgets.append(header)
+        field_labels = [
+            "Title of Organization",
+            "Profile Summary",
+            "Detailed Profile",
+            "Website",
+            "Street Address 1",
+            "Street Address 2",
+            "City",
+            "State/Province/Region",
+            "Zip Code",
+            "Email Address",
+            "Password",
+            "Password Again",
+        ]
+        for label in field_labels:
+            self.widgets.append(
+                Label(text=label, font_size=14)
+            )
+            self.widgets.append(
+                TextInput()
+            )
+
+        register_button = Button(text="Register")
+        register_button.bind(on_press=self.registerOrg)
+        self.widgets.append(register_button)
+
+        for widget in self.widgets:
+            self.add_widget(widget)
+
+    def registerOrg(self, *args):
+        with open("users.json", "r") as users_file:
+            users = json.load(users_file)
+
+            if self.widgets[2].text in users["users"]:
+                print("user already exists")
+            elif self.widgets[22].text == self.widgets[24].text:
+                with open("users.json", "w") as users_file:
+                    users["users"].append(
+                        {
+                            "org_title":self.widgets[2].text,
+                            "profile_summary":self.widgets[4].text,
+                            "detailed_profile":self.widgets[6].text,
+                            "website":self.widgets[8].text,
+                            "address_1":self.widgets[10].text,
+                            "address_2":self.widgets[12].text,
+                            "city":self.widgets[14].text,
+                            "state_province_region":self.widgets[16].text,
+                            "email_address":self.widgets[18].text,
+                            "password":self.widgets[20].text,
+                            "type":"ORG"
+                        }
+                    )
+                    json.dump(users, users_file)
 
 # page manager
 class Manager(App):
@@ -149,12 +267,12 @@ class Manager(App):
         screen.add_widget(self.sign_in_page)
         self.page_manager.add_widget(screen)
 
-        self.login_page = loginPage()
+        self.login_page = loginPage(orientation="vertical")
         screen = Screen(name="loginPage")
         screen.add_widget(self.login_page)
         self.page_manager.add_widget(screen)
 
-        self.register_page = registerPage()
+        self.register_page = registerPage(orientation="horizontal")
         screen = Screen(name="registerPage")
         screen.add_widget(self.register_page)
         self.page_manager.add_widget(screen)
